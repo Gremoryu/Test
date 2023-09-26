70c28b325b65d3d14bc24a2623f1264d53434a8b
Gremory08 <angel.soserr04@gmail.com>
2023-09-26 09:53:39 -0600

    Adicion de logica para la los socials

--- cv_web/src/components/atomos/socials.jsx
+++ cv_web/src/components/atomos/socials.jsx
@@ -1,17 +1,35 @@
 import { Box } from "@mui/material";
 import Icon from "@mdi/react";
 import { mdiFacebook, mdiInstagram, mdiGithub } from "@mdi/js";
-import './css/icons.css'
+import "./css/icons.css";
 
 import React from "react";
+import { Link } from "react-router-dom";
+
+const links = [
+  {
+    path: mdiFacebook,
+    url: "face",
+  },
+  {
+    path: mdiInstagram,
+    url: "insta",
+  },
+  {
+    path: mdiGithub,
+    url: "github",
+  },
+];
 
 export default function socials() {
   return (
     <>
       <Box>
-        <Icon className="icon" path={mdiFacebook} size={3} />
-        <Icon className="icon" path={mdiInstagram} size={3} />
-        <Icon path={mdiGithub} size={3} />
+        {links.map((link) => (
+          <Link to={link.url}>
+            <Icon className="icon" path={link.path} size={3} />
+          </Link>
+        ))}
       </Box>
     </>
   );
